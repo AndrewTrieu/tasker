2f3045eea00e1ea84e5bbd7db088ad0acce31857
Andrew Trieu <andrew.trieu@proton.me>
2024-10-31 16:20:52 +0200

    Initial front end

---
+++ tasker-client/tailwind.config.ts
@@ -0,0 +1,44 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: "#ffffff",
+        grey: {
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          400: "#9ca3af",
+          500: "#6b7280",
+          600: "#4b5563",
+          700: "#374151",
+          800: "#1f2937",
+        },
+        blue: {
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+        },
+        "dark-bg": "#101214",
+        "dark-secondary": "#1e2029",
+        "dark-tertiary": "#25262d",
+        "blue-primary": "#3b82f6",
+        "stroke-dark": "#2d3135",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
